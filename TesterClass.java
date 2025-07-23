2a16f5801486ee9f7ba75d3942c05e9a19e347cc
TrongBVD <buivoductrong1234@gmail.com>
2025-07-23 14:08:57 +0700

    Last Update TesterClass.java

--- TesterClass.java
+++ TesterClass.java
@@ -1,43 +1,42 @@
 
+/**
+ *
+ * @author Trong_DEV
+ */
 public class TesterClass extends Employee implements Salary {
 
-    protected double bonusRate;
-    protected String type;
-    protected double finalSal;
-    
-    
+    private double bonusRate;
+    private String type;
 
     public TesterClass(String empID, String empName, double baseSAl, double bonusRate, String type) {
         super(empID, empName, baseSAl);
         this.bonusRate = bonusRate;
         this.type = type;
-        this.finalSal = getSalaryForIT();
     }
 
     public double getBonusRate() {
         return bonusRate;
     }
 
-    public void setBonusRate(double bonusRate) {
-        this.bonusRate = bonusRate;
+    public void setBonusRate(double br) {
+        this.bonusRate = br;
     }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setType(String t) {
+        this.type = t;
     }
 
     @Override
     public double getSalaryForIT() {
-        return baseSAl + baseSAl * bonusRate;
+        return getBaseSAl() * bonusRate;
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", Bonus Rate: " + bonusRate + ", Type: " + type
-                + ", Final Salary: " + finalSal;
+        return super.toString() + ", BonusRate:" + bonusRate + ", Type:" + type;
     }
 }
